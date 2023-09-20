e31cb62c56e73d80e1c4e2a28311f3bc05a6e98c
Gesiel Neves <neves.gesiel@gmail.com>
2023-09-19 21:53:06 -0300

    update nodemon

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.use(express.static(__dirname));
-const server = app.listen(3000, () => {
- const { port } = server.address();
- console.log(`Listening on port ${port}`);
+
+const server = app.listen(5000, () => {
+  const { port } = server.address();
+  console.log(`Listening on port ${port}`);
 });
