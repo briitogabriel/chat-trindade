955704c229c9f4f231bb4786694aab1ada2ebd54
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2023-09-20 12:13:50 -0300

    Adicionando Model e Migration Chat

---
+++ src/models/Chat.js
@@ -0,0 +1,36 @@
+const { INTEGER, STRING } = require("sequelize");
+const { connection } = require("../database/connection");
+
+const Chat = connection.define("chats", {
+    chatId: {
+      type: INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: STRING,
+      allowNull: false,
+    },
+    description: {
+      type: STRING,
+    },
+    message: {
+      type: STRING,
+      allowNull: false,
+    },
+    userId: {
+        type: INTEGER,
+        references: {
+            model: {
+                tableName: "users"
+            }
+        }
+    }
+  },
+  {
+    undescored: true,
+    paranoid: true,
+  }
+);
+
+module.exports = { Chat };
